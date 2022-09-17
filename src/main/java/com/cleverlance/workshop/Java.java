cd70de8f769ad5dd395ea99f2400307817f0248e
Pavel Sveda <pavel.sveda@cleverlance.com>
2022-09-17 15:04:47 +0200

    Java Interoperability - void & Unit

---
+++ src/main/java/com/cleverlance/workshop/Java.java
@@ -0,0 +1,11 @@
+package com.cleverlance.workshop;
+
+public class Java {
+
+    void voidFunction() {
+    }
+
+    public static void main(String[] args) {
+        new Kotlin().unitFunction();
+    }
+}
---
+++ src/main/java/com/cleverlance/workshop/Kotlin.kt
@@ -0,0 +1,11 @@
+package com.cleverlance.workshop
+
+class Kotlin {
+
+    fun unitFunction() {
+    }
+}
+
+fun main() {
+    Java().voidFunction()
+}
