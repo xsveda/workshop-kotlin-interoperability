807e20a275b1c11cae168d3f9160bf019cb43498
Pavel Sveda <pavel.sveda@cleverlance.com>
2022-09-17 15:20:27 +0200

    Java Interoperability - nullability

--- src/main/java/com/cleverlance/workshop/Java.java
+++ src/main/java/com/cleverlance/workshop/Java.java
@@ -1,11 +1,20 @@
 package com.cleverlance.workshop;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class Java {
 
     static void voidFunction() {
     }
 
+    static @Nullable Integer intFunction(int a, @NotNull Integer b) {
+        return 0;
+    }
+
     public static void main(String[] args) {
         Kotlin.unitFunction();
+
+        Kotlin.intFunction(0, 0).longValue();
     }
 }
--- src/main/java/com/cleverlance/workshop/Kotlin.kt
+++ src/main/java/com/cleverlance/workshop/Kotlin.kt
@@ -5,6 +5,12 @@ package com.cleverlance.workshop
 fun unitFunction() {
 }
 
+fun intFunction(a: Int, b: Int?): Int? {
+    return 0
+}
+
 fun main() {
     Java.voidFunction()
+
+    Java.intFunction(0, 0)?.toLong() ?: error("Function did not return value")
 }
