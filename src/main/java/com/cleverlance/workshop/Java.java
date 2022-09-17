34d44f729d2c37537e18c13dab90b4bdcb98ef9b
Pavel Sveda <pavel.sveda@cleverlance.com>
2022-09-17 15:29:36 +0200

    Java Interoperability - (mutable)lists

--- src/main/java/com/cleverlance/workshop/Java.java
+++ src/main/java/com/cleverlance/workshop/Java.java
@@ -3,6 +3,9 @@ package com.cleverlance.workshop;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Java {
 
     static void voidFunction() {
@@ -12,9 +15,16 @@ public class Java {
         return 0;
     }
 
+    static @NotNull List<String> listFunction() {
+        return new ArrayList<>();
+    }
+
     public static void main(String[] args) {
         Kotlin.unitFunction();
 
         Kotlin.intFunction(0, 0).longValue();
+
+        Kotlin.listFunction().add("");
+        Kotlin.mutableListFunction().add("");
     }
 }
--- src/main/java/com/cleverlance/workshop/Kotlin.kt
+++ src/main/java/com/cleverlance/workshop/Kotlin.kt
@@ -9,8 +9,20 @@ fun intFunction(a: Int, b: Int?): Int? {
     return 0
 }
 
+fun listFunction(): List<String> {
+    return emptyList()
+}
+
+fun mutableListFunction(): MutableList<String> {
+    return mutableListOf()
+}
+
 fun main() {
     Java.voidFunction()
 
     Java.intFunction(0, 0)?.toLong() ?: error("Function did not return value")
+
+    Java.listFunction().add("")
+    //listFunction().add("")
+    mutableListFunction().add("")
 }
